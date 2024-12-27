9f54593156cb065573667bae49cd17c661cfbc6e
Praveen <yosoypraveen@gmail.com>
2024-12-27 21:12:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
-export default {
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
   server: {
     port: 8080, // Ensure Vite uses port 8080 on Render
     host: '0.0.0.0' // Ensure it's publicly accessible
   }
-}
+});
